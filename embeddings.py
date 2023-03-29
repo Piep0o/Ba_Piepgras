730229699211122dbcbffc01ccf32129ee6d99c8
Carl Piepgras <piepgras@stud.uni-hannover.de>
2023-03-29 21:16:43 +0200

    Add embeddings and mean-shift

---
+++ embeddings.py
@@ -0,0 +1,31 @@
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+from tqdm.auto import tqdm
+import os
+import pandas as pd
+# Initialize Img2Vec
+img2vec = Img2Vec()
+
+def return_embedding(img_path):
+    img = Image.open(img_path)
+    vec = img2vec.get_vec(img)
+    curr_df = pd.DataFrame(vec).T
+    return curr_df
+
+k = 0
+
+imgs_path = os.listdir('food-101_/images/')
+embedding_df = pd.DataFrame()
+
+for curr_img in tqdm(imgs_path):
+
+    try:
+        curr_df = return_embedding('food-101_/images/' + str(curr_img))
+        curr_df['img'] = curr_img
+        embedding_df = pd.concat([embedding_df, curr_df])
+    except:
+        print("Something went wrong with an img")
+        continue
+
+
+embedding_df.to_csv("foo.csv", mode='w', index=False)
